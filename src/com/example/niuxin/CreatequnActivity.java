d43918f913c1f389527266aa02644533f6ae184a
master8902 <master8902@163.com>
2015-07-12 09:47:26 +0800

    commit

---
+++ src/com/example/niuxin/CreatequnActivity.java
@@ -0,0 +1,12 @@
+package com.example.niuxin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreatequnActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.createqun);
+	}
+
+}
