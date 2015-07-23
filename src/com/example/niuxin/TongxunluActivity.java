5dc6a3babaf3ab2ff9b85d3e329e7fede246e1b8
ylh <ylh@163.com>
2015-07-24 01:03:49 +0800

    y

--- src/com/example/niuxin/TongxunluActivity.java
+++ src/com/example/niuxin/TongxunluActivity.java
@@ -5,52 +5,73 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.AdapterView.OnItemClickListener;
-import android.app.Activity;
-import android.content.Intent;
 
 public class TongxunluActivity extends Activity {
 	ListView listView;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//719修改
 		setContentView(R.layout.tongxunlu);
 		listView=(ListView)findViewById(R.id.zixuanlist);
-		
-		/*为ListView设置Adapter来绑定数据*/
-		SimpleAdapter zixuanAdapter= new SimpleAdapter(this, getData(),
-				R.layout.zixuanlistview, new String[]{"img","qunname","lastmes",
-						"time","quntag","renshu"},new int[]{R.id.img,R.id.qunname,
-			             R.id.lastmes,R.id.time,R.id.quntag,R.id.renshu});
+		SimpleAdapter zixuanAdapter= new SimpleAdapter(this, getData(),R.layout.zixuanlistview, 
+				new String[]{"image","title"},
+				new int[]{R.id.image,R.id.title});
 		listView.setAdapter(zixuanAdapter);
 		
-		/* 跳转到添加自选股界面 */
-		listView.setOnItemClickListener(new OnItemClickListener(){
+		//根据点击不同item跳转到自选股添加界面、公众号、查找用户及群组界面
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
-				// TODO Auto-generated method stub
-				startActivity(new Intent(TongxunluActivity.this,ChatActivity.class));
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				switch (position)
+				{
+				case 0: //点击第一个item
+					startActivity(new Intent(TongxunluActivity.this,zixuan_addActivity.class));
+					break;
+				}
 			}
+	
 		});
 	}
+
+
+
+	
+
 	private List<Map<String, Object>> getData() {
 		// TODO Auto-generated method stub
-		List<Map<String, Object>> listLiaoTian= new ArrayList<Map<String,Object>>();
+		List<Map<String, Object>> list= new ArrayList<Map<String,Object>>();
 		Map<String, Object> map = new HashMap<String, Object>();
 		
-		map.put("img", R.drawable.i1);
-		map.put("qunname", "中兴证券讨论组");
-		map.put("lastmes", "听说杨总又换女友了");
-		map.put("time","12:30");
-		map.put("quntag", "个股");
-		map.put("renshu", "12/20");
-		listLiaoTian.add(map);
-		return listLiaoTian;
+		map.put("image", R.drawable.addresslist_01);
+		map.put("title", "添加自选股");
+		list.add(map);
+		
+		map = new HashMap<String, Object>();
+		map.put("image", R.drawable.addresslist_02);
+		map.put("title", "公众号");
+		list.add(map);	
+		
+		map = new HashMap<String, Object>();
+		map.put("image", R.drawable.addresslist_03);
+		map.put("title", "查找用户及群组");
+		list.add(map);
+		
+		return list;
 	}
 
 }
+
 
