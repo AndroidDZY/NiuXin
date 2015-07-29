b9fbaebb5132bd130a41d92fd51f69ea4b2b9f72
ylh <ylh@163.com>
2015-07-30 07:24:12 +0800

    y

--- src/com/example/niuxin/edit_zixuanActivity.java
+++ src/com/example/niuxin/edit_zixuanActivity.java
@@ -1,13 +1,30 @@
 package com.example.niuxin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class edit_zixuanActivity extends Activity{
-	
+	ImageButton ibtn_add;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_zixuan);
+		
+		//获取ImageButton
+		ibtn_add = (ImageButton)this.findViewById(R.id.ibtn_edit_add);
+		//为ImageButton绑定点击事件
+		ibtn_add.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				startActivity(new Intent(edit_zixuanActivity.this,zixuan_addActivity.class));
+			}
+		});
+
 	}
 
 }
--- src/com/example/niuxin/zixuan_addActivity.java
+++ src/com/example/niuxin/zixuan_addActivity.java
@@ -2,6 +2,7 @@ package com.example.niuxin;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,21 +23,20 @@ import android.content.Intent;
 
 
 public class zixuan_addActivity extends Activity {
-	private int add_flag;
+	private int add_flag = R.drawable.add_flag01;
+	private Map<Integer,Integer> add_flags = new HashMap<Integer,Integer>();
 	private boolean isadd = false;
-	Button add_cancle,add_finish;
+	Button add_cancle, add_finish;
 	ListView listView;
+	List<Map<String, Object>> list = new LinkedList<Map<String, Object>>();
+	EditText gupiao;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.zixuan_add);
 		
 		// 获取EditText对象
-		EditText gupiao = (EditText) findViewById(R.id.et_gupiao);
-		//gupiao.setCursorVisible(false);//设置光标不显示,但不能设置光标颜色
-		//gupiao.clearFocus(); //让EditText失去焦点
-		//InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);  
-		//imm.hideSoftInputFromWindow(gupiao.getWindowToken(), 0);  
-		//gupiao.setInputType(InputType.TYPE_NULL); 
+		gupiao = (EditText) findViewById(R.id.et_gupiao);
 		
         // 获取Button对象  
         add_cancle=(Button)findViewById(R.id.add_cancle);
@@ -57,79 +57,84 @@ public class zixuan_addActivity extends Activity {
 
 			}
 		});
-        
+        list = getData();
 		//获取ListView
         listView = (ListView)findViewById(R.id.addlist);
-		SimpleAdapter addAdapter= new SimpleAdapter(this, getData(),R.layout.addlistview, 
+		SimpleAdapter addAdapter= new SimpleAdapter(this, list,R.layout.addlistview, 
 				new String[]{"name","num","add_flag"},
 				new int[]{R.id.name,R.id.num,R.id.add_flag});
 		listView.setAdapter(addAdapter);
 		
 		//实现点击不同的item，奇数偶数次点击更换imageview显示
 		listView.setOnItemClickListener(new OnItemClickListener() {
-
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				// TODO Auto-generated method stub
-				System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-				isadd = !isadd;
-				if (isadd == true) {
-					add_flag=R.drawable.add_flag02;
-				} else {
-					add_flag=R.drawable.add_flag01;
-				}
+			public void onItemClick(AdapterView<?> parent , View view , int position ,
+					long id ) {		
+				SimpleAdapter adapter=(SimpleAdapter)parent.getAdapter();
+	            Map<String,Object> map=(Map<String, Object>) adapter.getItem(position);
+	            if(Integer.valueOf(map.get("add_flag").toString())==R.drawable.add_flag01){
+	            	add_flag = R.drawable.add_flag02;
+	            }else
+	            	add_flag = R.drawable.add_flag01;
+	         //   map.put("add_flag", add_flag);
+	            list.get(position).put("add_flag", add_flag);        
+	            adapter.notifyDataSetInvalidated(); 
 			}
 		});
-
-	
-
 	}
 	
-	// 适配器将数据填入listview
-	private List<Map<String, Object>> getData() {
-		add_flag=R.drawable.add_flag01;
-		// TODO Auto-generated method stub
-		List<Map<String, Object>> list= new ArrayList<Map<String,Object>>();
-		Map<String, Object> 
-		
+	private List<Map<String, Object>> getList() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		Map<String, Object>
+
 		map = new HashMap<String, Object>();
-		map.put("name","海螺水泥");
+		map.put("id", 1);
+		map.put("name", "海螺水泥");
 		map.put("num", "600585");
-		map.put("add_flag",add_flag);
+		map.put("add_flag", add_flag);
 		list.add(map);
 		
 		map = new HashMap<String, Object>();
-		map.put("name","大秦铁路");
+		map.put("id", 2);
+		map.put("name", "大秦铁路");
 		map.put("num", "601006");
 		map.put("add_flag", add_flag);
-		list.add(map);	
-		
+		list.add(map);
+
 		map = new HashMap<String, Object>();
-		map.put("name","太钢不锈");
+		map.put("id", 3);
+		map.put("name", "太钢不锈");
 		map.put("num", "000825");
 		map.put("add_flag", add_flag);
 		list.add(map);
-		
+
 		map = new HashMap<String, Object>();
-		map.put("name","沪深300 ");
+		map.put("id", 4);
+		map.put("name", "沪深300 ");
 		map.put("num", "399300");
 		map.put("add_flag", add_flag);
 		list.add(map);
-		
+
 		map = new HashMap<String, Object>();
-		map.put("name","首创股份");
+		map.put("id", 5);
+		map.put("name", "首创股份");
 		map.put("num", "600008");
 		map.put("add_flag", add_flag);
-		list.add(map);	
-		
+		list.add(map);
+
 		map = new HashMap<String, Object>();
-		map.put("name","百大集团");
+		map.put("id", 6);
+		map.put("name", "百大集团");
 		map.put("num", "600865");
 		map.put("add_flag", add_flag);
 		list.add(map);
-		
+
 		return list;
 	}
-	
+
+	// 适配器将数据填入listview
+	private List<Map<String, Object>> getData() {		
+		List<Map<String, Object>> list = getList();
+		return list;
+	}
 }
