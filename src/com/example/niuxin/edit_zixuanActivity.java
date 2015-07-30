de4cf1676f491b2afcfefc2f04255ba99dae73d3
ylh <ylh@163.com>
2015-07-30 20:03:05 +0800

    y

--- src/com/example/niuxin/edit_zixuanActivity.java
+++ src/com/example/niuxin/edit_zixuanActivity.java
@@ -9,21 +9,36 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 public class edit_zixuanActivity extends Activity{
-	ImageButton ibtn_add;
+	Button btn_edit_add,btn_edit_cancle;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_zixuan);
 		
-		//获取ImageButton
-		ibtn_add = (ImageButton)this.findViewById(R.id.ibtn_edit_add);
-		//为ImageButton绑定点击事件
-		ibtn_add.setOnClickListener(new OnClickListener() {
+		//获取Button
+		btn_edit_add = (Button)findViewById(R.id.btn_edit_add11);
+		btn_edit_cancle = (Button)findViewById(R.id.btn_edit_cancle);
+		//为Button绑定点击事件
+		btn_edit_add.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0) {
+				finish();
+				//startActivity(new Intent(edit_zixuanActivity.this,zixuan_addActivity.class));
+			}
+		});
+
+		btn_edit_cancle.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-				startActivity(new Intent(edit_zixuanActivity.this,zixuan_addActivity.class));
+				// TODO Auto-generated method stub
+				finish();
+				
 			}
-		});
+		});	
+
+				
+				//startActivity(new Intent(edit_zixuanActivity.this,zixuan_addActivity.class));
+
 
 	}
 
