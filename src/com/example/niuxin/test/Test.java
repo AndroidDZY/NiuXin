1d48fe2c426c90ccb069cc079ed2b892db358319
dingliang <master8902@163.com>
2015-08-05 19:23:50 +0800

    dlcommit

--- src/com/example/niuxin/test/Test.java
+++ src/com/example/niuxin/test/Test.java
@@ -1,45 +1,16 @@
 package com.example.niuxin.test;
 
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import android.test.AndroidTestCase;
-
 import com.util.HttpPostUtil;
 import com.util.HttpPostUtil.OnReceiveDataListener;
 
 class TestResult implements OnReceiveDataListener{
 	//6.
 	@Override
-	public void onReceiveData(String strResult, int StatusCode) {
-		JSONArray jsonArray = null;
-        // 初始化list数组对象
-        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
-        try {
-			jsonArray = new JSONArray(strResult);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject jsonObject;
-			try {
-				jsonObject = jsonArray.getJSONObject(i);			
-            // 初始化map数组对象
-            HashMap<String, Object> map = new HashMap<String, Object>();
-            //获取具体的数据          
-				map.put("logo", jsonObject.getString("logo"));
-				list.add(map);
-			} catch (JSONException e) {			
-				e.printStackTrace();
-			}           
-            
-	}
+	public void onReceiveData(String strResult) {		
+		System.out.println("调用回调函数》》》》》》》》》》》》》》》》》》");			
 	}
 }
 
@@ -64,14 +35,9 @@ public class Test extends AndroidTestCase  {
 		postUtil.setUrl("http://172.20.1.120:8080/NiuXinServer/user/action_countAll.do");
 		postUtil.setOnReceiveDataListener (new TestResult());
 		//4.
-		postUtil.setRequest(postUtil.ADD, jsonObject);
+		postUtil.setRequest(jsonObject);
 		//5.
-		postUtil.execute();		
+		postUtil.execute();	
+		
 	}
-	
-	
-	
-	public static void main(String[] args) {
-	}
-
 }
