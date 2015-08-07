b83738c72b4d9a039ca2c11b58169d64bf5eacc8
unknown <616743998@qq.com>
2015-08-07 20:46:11 +0800

    zhou

--- src/com/example/niuxin/CreatequnActivity.java
+++ src/com/example/niuxin/CreatequnActivity.java
@@ -1,12 +1,30 @@
 package com.example.niuxin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class CreatequnActivity extends Activity {
+public class CreatequnActivity extends Activity  {
+	
+	Button buttonCancel,buttonDone;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.createqun);
+		buttonCancel=(Button)findViewById(R.id.createcancel);
+		buttonDone=(Button)findViewById(R.id.createdone);
+		buttonCancel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+		
 	}
-
+	
+	
 }
--- src/com/example/niuxin/SearchuserActivity.java
+++ src/com/example/niuxin/SearchuserActivity.java
@@ -2,14 +2,45 @@ package com.example.niuxin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class SearchuserActivity  extends Activity{
+public class SearchuserActivity  extends Activity implements OnClickListener{
+	
+	Button buttonCancel,buttonDone;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
 		setContentView(R.layout.searchuser);
+		buttonCancel=(Button)findViewById(R.id.searchuser_cancel);
+		buttonDone=(Button)findViewById(R.id.searchuser_done);
 		
 	}
 
+	@Override
+	public void onClick(View view) {
+		// TODO Auto-generated method stub
+		switch(view.getId()) {
+		case R.id.searchuser_cancel:
+			back();
+			break;
+		case R.id.searchuser_done:
+			done();
+			break;
+		}
+	}
+
+
+
+	private void done() {
+		// TODO Auto-generated method stub
+		finish();
+	}
+
+	private void back() {
+		// TODO Auto-generated method stub
+		finish();
+	}
 }
