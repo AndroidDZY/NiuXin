a11014f711ff9d42a225549c90bb98ac92fc9c5f
ylh <ylh@163.com>
2015-09-02 16:18:20 +0800

    ylh

--- src/com/example/niuxin/Tag_ManageActivity.java
+++ src/com/example/niuxin/Tag_ManageActivity.java
@@ -6,52 +6,108 @@ import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class Tag_ManageActivity extends Activity{
-	Button btn_tag_cancle,btn_tag_finish;
-	ImageButton btn_tag_add,btn_tag_delete;
+	Button btn_tag_cancle,btn_tag_add;
+	ImageButton btn_tag_delete;
 	ListView listView;
 	private int tag_flag = R.drawable.edit_flag01;
 	//实例化一个LinkedList类(LinkedList集合中的对象是一个个Map对象,而这个Map对象的键是String类型,值是Object类型)的对象list
 	List<Map<String, Object>> list = new LinkedList<Map<String, Object>>();
-	
+	//1
+	private SuoluetuActivity suolue;
+	public Handler handler = new Handler();
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
 		setContentView(R.layout.tag_manage);
-		
+		//2
+		suolue = new SuoluetuActivity(this,handler);
 		//获取Button
 		btn_tag_cancle = (Button)findViewById(R.id.btn_tag_cancle);
-		btn_tag_finish = (Button)findViewById(R.id.btn_tag_finish);
-		btn_tag_add = (ImageButton)findViewById(R.id.btn_tag_add);
+//		btn_tag_finish = (Button)findViewById(R.id.btn_tag_finish);
+		btn_tag_add = (Button)findViewById(R.id.btn_tag_add);
 		btn_tag_delete = (ImageButton)findViewById(R.id.btn_tag_delete);
 		
 		//为Button绑定点击事件
+		//取消按钮
 		btn_tag_cancle.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				finish();
 			}
 		});
-		btn_tag_finish.setOnClickListener(new OnClickListener() {
-			
+		
+		//新建标签按钮
+		btn_tag_add.setOnClickListener(new OnClickListener() {
+			View myView=LayoutInflater.from(getApplication()).inflate(R.layout.edittextview, null);//将layout对象转换为VIew对象
+			AlertDialog.Builder builder = new AlertDialog.Builder(Tag_ManageActivity.this);
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				finish();
+				 builder.setTitle("新建标签");
+				 //builder.setMessage("确定要删除这些标签吗？");
+				 builder.setView(myView);
+			       builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {  
+			           public void onClick(DialogInterface dialog, int whichButton) {  
+			               //这里添加点击确定后的逻辑  
+			              // showDialog("你选择了确定");  
+			           }  
+			       });  
+			       builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {  
+			           public void onClick(DialogInterface dialog, int whichButton) {  
+			               //这里添加点击确定后的逻辑  
+			               //showDialog("你选择了取消");  
+			           }  
+			       });  
+			       builder.create().show();  	
 			}
-		});	
+		});
+		
+		//删除标签按钮
+		btn_tag_delete.setOnClickListener(new OnClickListener() {
+			//创建一个对话框
+			AlertDialog.Builder builder = new AlertDialog.Builder(Tag_ManageActivity.this); 
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				//builder.setIcon(R.drawable.icon); 
+				
+			       //builder.setTitle("确定要删除这些标签吗？");
+			       builder.setMessage("确定要删除这些标签吗？");
+			       builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {  
+			           public void onClick(DialogInterface dialog, int whichButton) {  
+			               //这里添加点击确定后的逻辑  
+			              // showDialog("你选择了确定");  
+			           }  
+			       });  
+			       builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {  
+			           public void onClick(DialogInterface dialog, int whichButton) {  
+			               //这里添加点击确定后的逻辑  
+			               //showDialog("你选择了取消");  
+			           }  
+			       });  
+			       builder.create().show();  	
+			}
+		});
+		
+
 		
 		list = getData();// 填充list数据
 		listView = (ListView)findViewById(R.id.taglist);//获取ListView
