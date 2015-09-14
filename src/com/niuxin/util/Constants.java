bee5a0b279731f00eb0372d844b4d3b5b685fae6
dingliang <master8902@163.com>
2015-09-14 13:46:13 +0800

    dlcommit

--- src/com/niuxin/util/Constants.java
+++ src/com/niuxin/util/Constants.java
@@ -5,7 +5,7 @@ package com.niuxin.util;
 public class Constants {
 
 
-	public static final String SERVER_IP = "192.168.1.102"; // 服务器ip  115.28.242.139
+	public static final String SERVER_IP = "172.20.1.120"; // 服务器ip  115.28.242.139
 	public static final int SERVER_PORT = 8083;// 服务器端口
 
 	public static final int REGISTER_FAIL = 0;//注册失败
