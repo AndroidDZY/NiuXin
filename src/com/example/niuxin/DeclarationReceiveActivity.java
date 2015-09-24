1a4449ef513b2dd697406a2f1e7a1db12287de4f
ylh <ylh@163.com>
2015-09-25 00:47:10 +0800

    ylh

--- src/com/example/niuxin/DeclarationReceiveActivity.java
+++ src/com/example/niuxin/DeclarationReceiveActivity.java
@@ -3,13 +3,23 @@ package com.example.niuxin;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
 
 public class DeclarationReceiveActivity extends Activity{
+	private Button btnSetting, btnSource, btnType;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
 		setContentView(R.layout.declaration_receive);
+		
+		initView();
+	}
+
+	private void initView() {
+		//获取各种控件
+		btnSetting = (Button)findViewById(R.id.btn_setting);
+		
 	}
 
 }
