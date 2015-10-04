23cec334e89ee1f9a36cd1d4ce7d274e2df11c69
unknown <616743998@qq.com>
2015-10-04 09:42:02 +0800

    zhou

---
+++ src/com/example/niuxin/DeclarationDetailActivity.java
@@ -0,0 +1,59 @@
+package com.example.niuxin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class DeclarationDetailActivity extends Activity{
+	
+	RelativeLayout relativeLayoutModelChoice;
+	private Button buttonBack ,buttonSend ;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+		setContentView(R.layout.declaration_launch_detail);
+		
+		//数据初始化
+		relativeLayoutModelChoice=(RelativeLayout)findViewById(R.id.detail_model_control);
+		buttonBack=(Button)findViewById(R.id.detail_button_back);
+		buttonSend=(Button)findViewById(R.id.detail_button_send);
+		//设置监听事件
+		//模板选择跳转
+		
+		relativeLayoutModelChoice.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent(DeclarationDetailActivity.this,DeclarationModelChoiceActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		buttonBack.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+		
+		buttonSend.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				//获取数据，发送
+			}
+		});
+	}
+
+}
--- src/com/example/niuxin/DeclarationLaunchActivity.java
+++ src/com/example/niuxin/DeclarationLaunchActivity.java
@@ -1,16 +1,36 @@
 package com.example.niuxin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 
 public class DeclarationLaunchActivity extends Activity{
 	
+	Button iDeclarationDetail ;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
 		setContentView(R.layout.declaration_launch);
+		
+		//初始化
+		iDeclarationDetail=(Button)findViewById(R.id.declaration_launch_detail);
+		
+		//事件监听
+		//跳转到报单详情
+		iDeclarationDetail.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent =new Intent(DeclarationLaunchActivity.this ,DeclarationDetailActivity.class);
+				startActivity(intent);
+			}
+		});
 	}	
 
 }
