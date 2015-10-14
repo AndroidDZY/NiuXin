a27bb148cf94935b642beb51e64ae3a0c953becd
ylh <ylh@163.com>
2015-10-14 21:19:15 +0800

    ylh

--- src/com/example/niuxin/ContractTypeSelectActivity.java
+++ src/com/example/niuxin/ContractTypeSelectActivity.java
@@ -8,7 +8,7 @@ public class ContractTypeSelectActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
-		setContentView(R.layout.activity_declaration_source_select);
+		setContentView(R.layout.activity_contract_type_select);
 		
 		//initView();
 	}
