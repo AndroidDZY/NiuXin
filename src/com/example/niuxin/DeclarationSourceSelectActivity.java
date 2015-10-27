2d2aebb599c0a0d41f8d72627a51b6c8f8d83491
mushhappy <15995236009@163.com>
2015-10-27 11:28:46 +0800

    ylh

--- src/com/example/niuxin/DeclarationSourceSelectActivity.java
+++ src/com/example/niuxin/DeclarationSourceSelectActivity.java
@@ -8,8 +8,10 @@ import java.util.Map;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.AdapterView.OnItemClickListener;
@@ -23,20 +25,32 @@ public class DeclarationSourceSelectActivity extends Activity {
 	private int flagDeclarationOne = 0;
 	private int flagDeclarationTwo = 0;
 	private int flagDeclarationThree = 0;
+	private Button btnBack;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
 		setContentView(R.layout.activity_declaration_source_select);
 		
-		//initView();
+		initView();
+		
+		/* 按钮点击事件
+		 * 
+		 */
+		btnBack.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 		
 		list = getData();// 填充list数据
 		listView = (ListView)findViewById(R.id.lv_declaration_source_select);//获取ListView
 		//创建适配器
 		//第二个参数：list集合中的每一个Map对象对应生成一个列表项
-		//第三个参数：表明使用klinelist.xml文件作为列表项组件
+		//第三个参数：表明使用listview_declaration_source_select.xml文件作为列表项组件
 		//第四个参数：决定提取Map<String, Object>对象中的哪些key对应的value来生成列表项
-		//第五个参数：决定使用klinelist.xml文件中的哪些组件来填充列表项
+		//第五个参数：决定使用listview_declaration_source_select.xml文件中的哪些组件来填充列表项
 		SimpleAdapter adapter= new SimpleAdapter(this, list,R.layout.listview_declaration_source_select, 
 				new String[]{"flag","name"},
 				new int[]{R.id.iv_declaration_source_select_flag,R.id.tv_declaration_source_select_name});
@@ -68,7 +82,7 @@ public class DeclarationSourceSelectActivity extends Activity {
 				SimpleAdapter adapter=(SimpleAdapter)parent.getAdapter();//找到被点击的Adapter
 	            Map<String,Object> map=(Map<String, Object>) adapter.getItem(position);//找到被点击的列表项
 	            if(Integer.valueOf(map.get("flag").toString())==0){
-	            	flagDeclaration = R.drawable.k_flag;
+	            	flagDeclaration = R.drawable.ic_declaration_selected;
 	            }else
 	            	flagDeclaration = 0;
 	            //先清空
@@ -82,6 +96,11 @@ public class DeclarationSourceSelectActivity extends Activity {
 		});
 	}
 	
+	private void initView() {
+		// 控件初始化
+		btnBack = (Button)findViewById(R.id.btn_declaration_source_select_back);
+	}
+
 	private List<Map<String, Object>> getData() {
 		List<Map<String, Object>> list = getList();
 		return list;
@@ -97,19 +116,19 @@ public class DeclarationSourceSelectActivity extends Activity {
 		map = new HashMap<String, Object>();
 		map.put("id", 2);
 		map.put("flag", flagDeclarationOne);
-		map.put("name", "报单者1");
+		map.put("name", "已关注的报单者");
 		list.add(map);
 
 		map = new HashMap<String, Object>();
 		map.put("id", 3);
 		map.put("flag", flagDeclarationTwo);
-		map.put("name", "报单者2");
+		map.put("name", "报单者1");
 		list.add(map);
 		
 		map = new HashMap<String, Object>();
 		map.put("id", 4);
 		map.put("flag", flagDeclarationThree);
-		map.put("name", "报单者3");
+		map.put("name", "报单者2");
 		list.add(map);
 
 		return list;
