f6387e323215bb31bdd5fbba0d438cf781ed3c92
dingliang <master8902@163.com>
2015-11-16 17:39:47 +0800

    dl

--- src/com/example/niuxin/ContractTypeSelectActivity.java
+++ src/com/example/niuxin/ContractTypeSelectActivity.java
@@ -32,12 +32,12 @@ public class ContractTypeSelectActivity extends Activity {
 	private ListView listView;
 	List<Map<String, Object>> list = new LinkedList<Map<String, Object>>();
 	SimpleAdapter adapter = null;
-private Set<Integer> contractSelectList = new HashSet<Integer>();
+	private Set<Integer> contractSelectList = new HashSet<Integer>();
 	private Button btnBack;
 	private Button btnSave;
 	private Handler handler = new Handler();
 	private SharePreferenceUtil util = null;
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -46,91 +46,91 @@ private Set<Integer> contractSelectList = new HashSet<Integer>();
 		thread.start();
 
 	}
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+		requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉标题栏
 		setContentView(R.layout.activity_contract_type_select);
 		util = new SharePreferenceUtil(this, Constants.SAVE_USER);
 		initView();
-	
-		listView = (ListView)findViewById(R.id.lv_contract_type_select);//获取ListView
-		//创建适配器
-		//第二个参数：list集合中的每一个Map对象对应生成一个列表项
-		//第三个参数：表明使用listview_contract_type_select.xml文件作为列表项组件
-		//第四个参数：决定提取Map<String, Object>对象中的哪些key对应的value来生成列表项
-		//第五个参数：决定使用listview_contract_type_select.xml文件中的哪些组件来填充列表项
 
-		adapter= new SimpleAdapter(this, list,R.layout.listview_contract_type_select, 
-				new String[]{"flag","type"},
-				new int[]{R.id.iv_contract_type_select_flag,R.id.tv_contract_type_select_name});
-		listView.setAdapter(adapter);//为listView设置适配器
-		
-		//实现点击不同的item，奇数偶数次点击更换imageview显示
+		listView = (ListView) findViewById(R.id.lv_contract_type_select);// 获取ListView
+		// 创建适配器
+		// 第二个参数：list集合中的每一个Map对象对应生成一个列表项
+		// 第三个参数：表明使用listview_contract_type_select.xml文件作为列表项组件
+		// 第四个参数：决定提取Map<String, Object>对象中的哪些key对应的value来生成列表项
+		// 第五个参数：决定使用listview_contract_type_select.xml文件中的哪些组件来填充列表项
+
+		adapter = new SimpleAdapter(this, list, R.layout.listview_contract_type_select, new String[] { "flag", "type" },
+				new int[] { R.id.iv_contract_type_select_flag, R.id.tv_contract_type_select_name });
+		listView.setAdapter(adapter);// 为listView设置适配器
+
+		// 实现点击不同的item，奇数偶数次点击更换imageview显示
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> parent , View view , int position ,
-					long id ) {				
-				SimpleAdapter adapterClick=(SimpleAdapter)parent.getAdapter();//找到被点击的Adapter
-	            Map<String,Object> map=(Map<String, Object>) adapterClick.getItem(position);//找到被点击的列表项
-	            int flag= 0;
-	            if(Integer.valueOf(map.get("flag").toString())==0){	            
-	            	flag = R.drawable.ic_declaration_selected;
-	            	contractSelectList.add((Integer)(list.get(position).get("id")));
-	            }
-	            else{
-	            	flag= 0;
-	            	contractSelectList.remove((Integer)(list.get(position).get("id")));
-	            }
-	            	
-	            list.get(position).put("flag", flag);//将更新过的flag值放入list中        
-	           
-	            adapterClick.notifyDataSetInvalidated();//使更新过的list数据生效
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				SimpleAdapter adapterClick = (SimpleAdapter) parent.getAdapter();// 找到被点击的Adapter
+				Map<String, Object> map = (Map<String, Object>) adapterClick.getItem(position);// 找到被点击的列表项
+				int flag = 0;
+				if (Integer.valueOf(map.get("flag").toString()) == 0) {
+					flag = R.drawable.ic_declaration_selected;
+					contractSelectList.add((Integer) (list.get(position).get("id")));
+				} else {
+					flag = 0;
+					contractSelectList.remove((Integer) (list.get(position).get("id")));
+				}
+
+				list.get(position).put("flag", flag);// 将更新过的flag值放入list中
+
+				adapterClick.notifyDataSetInvalidated();// 使更新过的list数据生效
 			}
 		});
 	}
-	
+
 	private void initView() {
 		// 控件初始化
-		btnBack = (Button)findViewById(R.id.btn_contract_type_select_back);
-		btnSave  = (Button)findViewById(R.id.btn_contract_type_select_save);
-		
-		/* 按钮点击事件
+		btnBack = (Button) findViewById(R.id.btn_contract_type_select_back);
+		btnSave = (Button) findViewById(R.id.btn_contract_type_select_save);
+
+		/*
+		 * 按钮点击事件
 		 * 
 		 */
-		btnBack.setOnClickListener(new OnClickListener() {			
+		btnBack.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v) {				
-			/*	String result = "";
-				if(contractSelectList!=null){
-					int b = contractSelectList.toString().length();
-					result = contractSelectList.toString().substring(1,b-1);
-				}
-				Intent intentType = new Intent(ContractTypeSelectActivity.this, DeclarationReceiveActivity.class);
-				intentType.putExtra("contract",result);
-				startActivity(intentType);*/
+			public void onClick(View v) {
+				/*
+				 * String result = ""; if(contractSelectList!=null){ int b =
+				 * contractSelectList.toString().length(); result =
+				 * contractSelectList.toString().substring(1,b-1); } Intent
+				 * intentType = new Intent(ContractTypeSelectActivity.this,
+				 * DeclarationReceiveActivity.class);
+				 * intentType.putExtra("contract",result);
+				 * startActivity(intentType);
+				 */
 				finish();
 			}
-		});	
-		
-		btnSave.setOnClickListener(new OnClickListener() {			
+		});
+
+		btnSave.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v) {				
+			public void onClick(View v) {
 				String result = "";
-				if(contractSelectList!=null){
+				if (contractSelectList != null) {
 					int b = contractSelectList.toString().length();
-					result = contractSelectList.toString().substring(1,b-1);
+					result = contractSelectList.toString().substring(1, b - 1);
 				}
 				Intent intentType = new Intent(ContractTypeSelectActivity.this, DeclarationReceiveActivity.class);
-				intentType.putExtra("contract",result);
+				intentType.putExtra("contract", result);
 				startActivity(intentType);
 				finish();
 			}
-		});	
+		});
 	}
 
 	class SearchThread extends Thread {
 		@Override
-		public void run() {			
+		public void run() {
 			// 新建工具类，向服务器发送Http请求
 			HttpPostUtil postUtil = new HttpPostUtil();
 			JSONArray jArray = new JSONArray();
@@ -138,12 +138,12 @@ private Set<Integer> contractSelectList = new HashSet<Integer>();
 			// 向服务器发送数据，如果没有，可以不发送 JSONObject jsonObject = new JSONObject();
 			try {
 				jsonObject.put("id", util.getId());
-			
+
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
 			jArray.put(jsonObject);
-		
+
 			// 设置发送的url 和服务器端的struts.xml文件对应
 			postUtil.setUrl("/contract/contract_selectAll.do");
 			// 向服务器发送数据
@@ -158,26 +158,27 @@ private Set<Integer> contractSelectList = new HashSet<Integer>();
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-			list.clear();
-			
-			for (int i = 0; i < jsonArray.length(); i++) {
-				try {
-					JSONObject myjObject = jsonArray.getJSONObject(i);// 获取每一个JsonObject对象
-					Map<String, Object> map = new HashMap<String, Object>();
-					// 获取每一个对象中的值
-					int id = myjObject.getInt("id");
-					String type = myjObject.getString("type");
-					map.put("id", id);				
-					map.put("type", type);	
-					map.put("flag", 0);
-					list.add(map);
-				} catch (JSONException e) {
-					e.printStackTrace();
+			if ( null!= list)
+				list.clear();
+			if (null != jsonArray)
+				for (int i = 0; i < jsonArray.length(); i++) {
+					try {
+						JSONObject myjObject = jsonArray.getJSONObject(i);// 获取每一个JsonObject对象
+						Map<String, Object> map = new HashMap<String, Object>();
+						// 获取每一个对象中的值
+						int id = myjObject.getInt("id");
+						String type = myjObject.getString("type");
+						map.put("id", id);
+						map.put("type", type);
+						map.put("flag", 0);
+						list.add(map);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
 				}
-			}
 			Runnable r = new Runnable() {
 				@Override
-				public void run() {					
+				public void run() {
 					adapter.notifyDataSetChanged();
 				}
 
@@ -186,7 +187,4 @@ private Set<Integer> contractSelectList = new HashSet<Integer>();
 		}
 	}
 
-	
-	
 }
-
